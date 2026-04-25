215bc3daa894167f5751b60b98f20ef6d4aceeff
Imamiablo <imamiablo.sm@gmail.com>
2026-04-25 14:30:37 +0100

    0.16

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from pathlib import Path
 from typing import Any
 
 from fastapi import FastAPI
-from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 from src.config import AppConfig
+from src.db import Database
 from src.services.article_service import ArticleService
+from src.services.furigana_service import FuriganaService
 from src.services.transcript_service import TranscriptService
 from src.services.vocab_service import VocabService
 from src.services.llm_service import LLMService
@@ -20,7 +24,7 @@ app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 config = None
-db = None
+db = Database(config.db_path)
 furigana_service = None
 
 def build_article_service(api_key: str, base_url: str, model: str) -> tuple[ArticleService, TranscriptService]:
